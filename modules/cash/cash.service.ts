491560b590ccd7d640dea11e4762eddd86d79c21
Abdullah Adel <abdullah@gowell.co>
2026-01-25 23:03:07 +0200

    feat: implement withdraw function with transaction handling and balance checks in cash service

--- modules/cash/cash.service.ts
+++ modules/cash/cash.service.ts
@@ -5,6 +5,7 @@ import {
   MoneyMovementsInsertType,
 } from "@/src/database/scheme/cash.schema";
 import { withTransaction } from "@/src/database/transaction";
+import Decimal from "decimal.js";
 import { eq, sql } from "drizzle-orm";
 
 export async function deposit({
@@ -39,6 +40,51 @@ export async function deposit({
       .where(eq(accountBalances.accountId, accountId))
       .returning();
 
+    return updated;
+  });
+}
+
+export async function withdraw({
+  amount,
+  accountId,
+  reason,
+}: Pick<MoneyMovementsInsertType, "accountId" | "amount" | "reason">) {
+  return withTransaction(async (tx) => {
+    if (Decimal(amount).gte(0)) {
+      throw new Error("AMOUNT MUST BE NEGATIVE");
+    }
+
+    const {
+      rows: [account],
+    } = await tx.execute<AccountBalancesSelectType>(sql`
+      SELECT * 
+      FROM ${accountBalances}
+      WHERE ${accountBalances.accountId} = ${accountId}
+      FOR UPDATE
+      `);
+
+    if (!account) {
+      throw new Error("ACCOUNT_NOT_FOUND");
+    }
+    const nextBalance = Decimal(account.balance).plus(amount);
+    if (nextBalance.isNegative()) {
+      throw new Error("INSUFFICIENT_FUNDS");
+    }
+
+    await tx.insert(moneyMovements).values({
+      accountId: accountId,
+      amount: amount,
+      reason: reason,
+      refType: "withdraw",
+    });
+
+    const [updated] = await tx
+      .update(accountBalances)
+      .set({
+        balance: sql`${accountBalances.balance} + ${amount}`,
+      })
+      .where(eq(accountBalances.accountId, accountId))
+      .returning();
     return updated;
   });
 }
